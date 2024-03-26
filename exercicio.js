fc2a8d7ebda4a89af336ffe31671f7ba28b3ead3
Gustavo Dias <gustavogdcosta@hotmail.com>
2024-03-26 09:03:48 -0300

    primeiro commit

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function Multiplica(a, b) {
+    return a * b;
+}
+function nome(Nomedapessoa) {
+    return Nomedapessoa;
+}
+console.log(Multiplica(10, 2));
+console.log(`olá ` + nome('Gustavo'));
---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function Multiplica (a:number, b:number): number {
+    return a*b;
+}
+
+function nome(Nomedapessoa:string):string {
+    return Nomedapessoa;
+}
+
+console.log (Multiplica (10,2));
+console.log (`olá ` + nome('Gustavo'));
